5f5bed6de6ca84a0ae19542d86c8b6a8fe183cc4
Bartek <bartektrybalaa@gmail.com>
2020-10-18 17:01:18 +0200

    First commit. Init file. Add pictures of figures.

---
+++ ChessEngine.py
@@ -0,0 +1,4 @@
+"""
+This class is responsible for storing all the information about the current state of a chess game.
+It will also be responsible for detemining the valid moves at the current state. It will also keep a move log.
+"""
---
+++ ChessMain.py
@@ -0,0 +1,4 @@
+"""
+This is our main driver filer. 
+It will responsible for handling user input and displaying the current GameState object.
+"""
