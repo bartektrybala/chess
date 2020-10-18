84b759e684a5a545202dcd46c7ff9b4a56d65bec
Bartek <bartektrybalaa@gmail.com>
2020-10-18 17:39:27 +0200

    Print chess board in console

--- ChessMain.py
+++ ChessMain.py
@@ -2,3 +2,35 @@
 This is our main driver filer. 
 It will responsible for handling user input and displaying the current GameState object.
 """
+
+import pygame as p
+import ChessEngine
+
+WIDHT = HEIGHT = 512    #400 is another option
+DIMENSION = 8           #dimentions of a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15            #for animations later on
+IMAGES = {}
+
+"""
+Initialize a global dictionary of images. This will be called exactly once in the main
+"""
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load('images/' + piece + '.png'), (SQ_SIZE, SQ_SIZE))
+    #Note: we can access an image by saying 'IMAGES['wp']'
+
+'''
+The main driver for our code. This will handle user input and updating the graphics
+'''
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDHT, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color('white'))
+    gs = ChessEngine.GameState()
+    print(gs.board)
+
+main()
